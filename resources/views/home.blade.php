d180dcfdbc62c5501fb08966ef9996bac6886f6b
robert-lsi <robert@lsi.co.uk>
2018-09-19 16:02:06 +0100

    test
    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,31 +3,36 @@
 @section('content')
 
 <div class="container">
-    <h1>LSi Print Creator - Login</h1>
+    <h1>LSi Print Creatorrrrr</h1>
     
     <hr>
     
-    {{ Form::open() }}
-    <h1>Login</h1>
+    @if(session('accesscode') !== env('ACCESS_CODE'))
+        {{ Form::open() }}
+        <h1>Login Here</h1>
 
-    <!-- if there are login errors, show them here -->
-    <p>
-        {{ $errors->first('email') }}
-        {{ $errors->first('password') }}
-    </p>
+        <!-- if there are login errors, show them here -->
+        <p>
+            {{ $errors->first('email') }}
+            {{ $errors->first('password') }}
+        </p>
 
-    <p>
-        {{ Form::label('email', 'Email Address') }}
-        {{ Form::text('email', Input::old('email'), array('placeholder' => 'awesome@awesome.com')) }}
-    </p>
+        <p>
+            {{ Form::label('email', 'Email Address') }}
+            {{ Form::text('email', Input::old('email'), array('placeholder' => 'awesome@awesome.com')) }}
+        </p>
 
-    <p>
-        {{ Form::label('password', 'Password') }}
-        {{ Form::password('password') }}
-    </p>
+        <p>
+            {{ Form::label('password', 'Password') }}
+            {{ Form::password('password') }}
+        </p>
 
-    <p>{{ Form::submit('Submit!') }}</p>
-    {{ Form::close() }}
+        <p>{{ Form::submit('Submit!') }}</p>
+        {{ Form::close() }}
+    @else
+        <h3>You are logged in</h3>
+        <p><a href="{{ action('ProductController@index') }}" class="btn btn-md btn-primary">Click to view the product list</a></p>
+    @endif
     
     
 </div>
