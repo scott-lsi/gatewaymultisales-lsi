4927b8e6a9825893731a629d4c8fa91d23e76b81
robert-lsi <robert@lsi.co.uk>
2018-09-21 13:56:51 +0100

    Test email changes
    Test email changes to CartController

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -123,6 +123,9 @@ class CartController extends Controller
         $email_data = [
             'name' => $request->name,
             'email' => $request->email,
+            'custnumber' => $request->custnumber,
+            'custname' => $request->custname,
+            'moreinfo' => $request->moreinfo,
         ];
         
         // save it all and send things
