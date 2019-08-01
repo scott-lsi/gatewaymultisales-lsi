ff13903b97c94d5e6ceabd978ba29a3fc6eb62a3
Robert Hemingway <rhemingway@lsi.local>
2019-08-01 09:09:30 +0100

    Finishes for the report
    Finishes for the report feature. Changes so it sends the correct details.

--- resources/views/emails/report.blade.php
+++ resources/views/emails/report.blade.php
@@ -7,20 +7,12 @@
 <tr>
 	<td>
         <p style="font-size:14px; margin-top:30px; margin-bottom:20px;">Hi,</p>
-        <p style="font-size:14px; margin-top:10px; margin-bottom:20px;">Please find attached the report for the previous month for J&auml;germeister labels</p>
+        <p style="font-size:14px; margin-top:10px; margin-bottom:20px;">Please find attached the report for the previous months Samples</p>
 	</td>
 </tr>
 <tr>
 	<td>
 		<img src="{{ asset('images/emails/hr.png') }}" style="display:block" border="0" />
-	</td>
-</tr>
-<tr>
-	<td>
-        <p style="font-size:11px; margin-top:30px; margin-bottom:20px;">
-            AWRS No: XMAW00000100010<br />
-            VAT Registration No: GB168857155
-        </p>
 	</td>
 </tr>
 
