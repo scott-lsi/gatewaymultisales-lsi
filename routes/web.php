997a101cb3c1e41466aa3563c90ed9410cdb697f
Scott Brown <sbrown@lsi.co.uk>
2020-10-05 14:12:41 +0100

    laravel v8 upgrade

--- routes/web.php
+++ routes/web.php
@@ -24,11 +24,11 @@ Route::get('/products', 'ProductController@index');
 Route::get('/products/trashed', 'ProductController@getTrashed');
 Route::get('/products/{type}', 'ProductController@getProductsByType');
 Route::resource('product', 'ProductController');
-Route::get('product/personaliser/{id}/{gatewaymulti?}', 'ProductController@personaliser');
+Route::get('product/personaliser/{productId}/{row_id_to_update}/{this_g3d_id}/{printJobRef?}', 'ProductController@personaliser');
 Route::get('productepa/{id?}', 'ProductController@getExternalPricingAPI');
 
 Route::get('basket', 'CartController@index');
-Route::post('basket/add/{gatewaymulti?}', 'CartController@add');
+Route::post('basket/add/{row_id_to_update?}/{next_g3d_id?}/{print_job_id_to_update?}','CartController@add');
 Route::get('basket/destroy', 'CartController@destroy');
 Route::get('basket/redir/{id?}/{gatewaymultiId?}', 'CartController@gatewayRedir');
 Route::get('basket/remove-item/{rowId}', 'CartController@getRemoveItem');
